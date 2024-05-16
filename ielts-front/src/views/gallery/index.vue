ea0197783fb431c7b20c9a75f18d1b7dc836a509
shenweijian <shenweijian@zhufaner.com>
2024-05-16 18:34:10 +0800

    开发

--- ielts-front/src/views/gallery/index.vue
+++ ielts-front/src/views/gallery/index.vue
@@ -5,10 +5,26 @@
         <SvgIcon name="atx" class="hidden md:block" width="180" height="80" />
       </a>
       <el-menu default-active="/main/book" class="el-menu-demo" mode="horizontal" :ellipsis="false" router>
-        <el-menu-item index="/main/book">单词书</el-menu-item>
-        <el-menu-item index="/main/statistics">统计</el-menu-item>
-        <el-menu-item index="/main/errorbook">错词本</el-menu-item>
-        <el-menu-item index="/main/user">我的</el-menu-item>
+        <el-menu-item index="/home">
+          <el-icon><Headset /></el-icon>
+          听写
+        </el-menu-item>
+        <el-menu-item index="/main/book">
+          <el-icon><Memo /></el-icon>
+          单词书
+        </el-menu-item>
+        <el-menu-item index="/main/statistics">
+          <el-icon><DataLine /></el-icon>
+          统计
+        </el-menu-item>
+        <el-menu-item index="/main/errorbook">
+          <el-icon><DocumentDelete /></el-icon>
+          错词本
+        </el-menu-item>
+        <el-menu-item index="/main/user">
+          <el-icon><User /></el-icon>
+          我的
+        </el-menu-item>
       </el-menu>
     </div>
     <router-view />
@@ -17,4 +33,5 @@
 </template>
 <script setup>
   import Footer from '@/components/footer/index.vue';
+  import { Memo, Headset, DataLine, DocumentDelete, User } from '@element-plus/icons-vue';
 </script>
