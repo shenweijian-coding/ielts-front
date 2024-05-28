8ffcc7657c73997991ae0d16caae5cc7c69713af
shenweijian <shenweijian@zhufaner.com>
2024-05-28 15:22:33 +0800

    开发

--- ielts-front/src/views/user/index.vue
+++ ielts-front/src/views/user/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="relative mt-auto flex w-full flex-1 flex-col overflow-y-auto lg:px-30 lg:pt-6 mb-10 md:mb-auto bg-white">
-    <div class="">
-      <el-button @click="handleLogout" type="" class="mt-10">退出登陆</el-button>
-      <!-- <el-button @click="handleLogout" type="" class="mt-10">清除缓存</el-button>
+  <div class="relative flex lg:px-30 align-center justify-center">
+    <el-button @click="handleLogout" type="" class="mt-10">退出登陆</el-button>
+    <!-- <el-button @click="handleLogout" type="" class="mt-10">清除缓存</el-button>
       <el-button @click="handleLogout" type="" class="mt-10">修改密码</el-button> -->
-    </div>
     <tabbar />
   </div>
 </template>
